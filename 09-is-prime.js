9af10e99088f8f1724dcbdf122ba8cf365c06bff
SecretHelper <46404272+SecretHelper@users.noreply.github.com>
2023-03-13 15:27:17 -0500

    09-isPrime-a/A-w11-2023-03-13-Mon

--- 09-is-prime.js
+++ 09-is-prime.js
@@ -5,6 +5,20 @@ Otherwise, false. A number is prime if it is only divisible by 1 and itself.
 
 // Your code here
 
+let isPrime = function(num) {
+	if (num < 2) {
+    	return false;
+    }
+  let i = 2;
+  while (i < num) {
+  	if (num % i === 0) {
+    return false;
+    }
+    i++;
+  }
+return true;
+}
+
 console.log(isPrime(2)); // => true
 console.log(isPrime(10)); // => false
 console.log(isPrime(11)); // => true
